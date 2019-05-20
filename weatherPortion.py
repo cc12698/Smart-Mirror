815c99aa0d262de72c8a75435b111b1197fa748e
Chris <guitar12698@hotmail.com>
2019-05-19 20:10:35 -0400

    five day forecast

--- weatherPortion.py
+++ weatherPortion.py
@@ -3,8 +3,16 @@ import requests
 api_address = 'http://api.openweathermap.org/data/2.5/weather?appid=d16e105a8c04388c73b5429744f161d8&q='#New%20York
 city = input("City Name: ")
 
-url = api_address + city
+url_current = api_address + city
 
-json_data = requests.get(url).json()
+json_data = requests.get(url_current).json()
 
 print(json_data)
+
+five_day = 'http://api.openweathermap.org/data/2.5/forecast?appid=d16e105a8c04388c73b5429744f161d8&q=New%20York,%20US'
+
+five_day_json = requests.get(five_day).json()
+
+print(five_day_json)
+
+
